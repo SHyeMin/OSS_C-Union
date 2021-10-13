ee39b3dffa1e438e7f0208dc609134e6068abbb3
blacksg <76660052+blacksg@users.noreply.github.com>
2021-10-13 17:51:47 +0900

    Update main.c with new line and text correction
    Corrected printf() in line 12, and added new line in line 14

--- main.c
+++ main.c
@@ -9,8 +9,9 @@ int main()
   printf("> SHyeMin: Hello! I'm Suh Hye-Min the teamleader of the team C-Union.\n");
   printf("I'm looking forward to working with you on the project!\n");
   line;
-  printf("> blacksg: Hello, I'm blacksg.\n");
+  printf("> blacksg: Hello, I'm Seok-wan Kang, a member of team C-Union.\n");
   printf("I'm currently studying about Android, and want to be skilled at using GitHub.");
+  line;
   
   return 0;
 }
