0ff68b0c16d2830fde395192e276d893e998e02c
Suh Hye-Min <92361486+SHyeMin@users.noreply.github.com>
2021-10-13 17:10:19 +0900

    Create main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define line printf("------------------------------------------------------------------------\n");
+
+int main()
+{
+  printf(">> Welcome to our first project! <<\n");
+  printf("* Please leave your name and simple message for our team!\n");
+  line;
+  printf("> SHyeMin: Hello! I'm Suh Hye-Min the teamleader of the team C-Union.\n");
+  printf("I'm looking forward to working with you on the project!\n");
+  
+  return 0;
+}
